22c310ed9422f4bc2aa034e423e5be9149eec68d
Rondinele Guimaraes <rondinele.guimaraes@baritecnologia.com.br>
2022-04-19 10:54:31 -0300

    Start

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace furry_enigma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
